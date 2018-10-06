274b23b2b648c0cb8c85a36854e2a83379b365a8
Samuel46 <samuelmunyiri34@gmail.com>
2018-10-07 07:59:02 +1300

    Updates

---
+++ precache-manifest.bcd7b27a59869a6ce0974cc1b72ebfc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a6456428d717375188e",
+    "url": "/lyrics/static/js/runtime~main.9a645642.js"
+  },
+  {
+    "revision": "c6e6450a5c84a924bc81",
+    "url": "/lyrics/static/js/main.c6e6450a.chunk.js"
+  },
+  {
+    "revision": "cb077c9c3157bd02c83e",
+    "url": "/lyrics/static/js/1.cb077c9c.chunk.js"
+  },
+  {
+    "revision": "c6e6450a5c84a924bc81",
+    "url": "/lyrics/static/css/main.9743dcc1.chunk.css"
+  },
+  {
+    "revision": "89387d6f6b882638c3cbf8fa8a1a974f",
+    "url": "/lyrics/index.html"
+  }
+];
